55c5d746c13ef9380691d12a76421599c17d8cc2
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-10 01:17:25 +0700

    feat: tambahkan validasi input dan pengecekan pengguna yang terdaftar di fungsi register

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,8 +6,26 @@ const JWT_SECRET = process.env.JWT_SECRET || 'jwt_secret';
 
 exports.register = async (req, res) => {
     try {
+        const { nama, email, password, role } = req.body;
 
+        if (!nama || !email || !password) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'Nama, email, dan password wajib diisi'
+            });
+        }
+
+        const [existingUser] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
+        if (existingUser.length > 0) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'Email sudah terdaftar'
+            });
+        }        
     } catch (error) {
-        
+        res.status(500).json({
+            status: 'error',
+            message: 'Terjadi kesalahan pada server'
+        });
     }
 };
