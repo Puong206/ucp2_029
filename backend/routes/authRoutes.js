a34d5f621a68333bc92623ec8168afc01b098101
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-10 00:24:29 +0700

    feat: tambahkan route untuk autentikasi

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.router();
+
+const authController = require('../controllers/authController');
+
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+router.post('/logout', authController.logout);
+router.get('/me', authController.getMe);
+
+module.exports = router;
