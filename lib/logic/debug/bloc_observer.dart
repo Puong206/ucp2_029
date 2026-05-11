27977c244c35d4039379af1b23965e1604af8ed3
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-11 15:41:08 +0700

    feat: tambahkan observer BLoC untuk logging event, perubahan, dan error

---
+++ lib/logic/debug/bloc_observer.dart
@@ -0,0 +1,34 @@
+import 'dart:developer' as developer;
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    developer.log(
+      'Event: ${event.runtimeType}', 
+      name: 'BLoC'
+    );
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    developer.log(
+      'Change: ${change.runtimeType} -> ${change.nextState.runtimeType}',
+      name: 'BLoC',
+    );
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    developer.log(
+      'Error: ${error.toString()}',
+      name: 'BLoC',
+      error: error,
+      stackTrace: stackTrace,
+    );
+    super.onError(bloc, error, stackTrace);
+  }
+}
