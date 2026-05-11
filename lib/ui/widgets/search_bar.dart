f2301b284fabbcef1b91abf42e4187eeaa812e15
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-12 00:54:19 +0700

    feat: tambahkan widget SearchBar untuk pencarian

---
+++ lib/ui/widgets/search_bar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SearchBar extends StatelessWidget {
+  final TextEditingController controller;
+  final Function(String) onChanged;
+  final Function()? onFilterTap;
+
+  const SearchBar({
+    Key? key,
+    required this.controller,
+    required this.onChanged,
+    this.onFilterTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 8,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: TextField(
+        controller: controller,
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          hintText: 'Pencarian....',
+          hintStyle: TextStyle(color: Color(0xFFCCCCCC)),
+          prefixIcon: Icon(Icons.search, color: Color(0xFFCCCCCC)),
+          suffixIcon: GestureDetector(
+            onTap: onFilterTap,
+            child: Container(
+              margin: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Color(0xFF1A1A2E0),
+                shape: BoxShape.circle,
+              ),
+              child: Icon(Icons.tune, color: Colors.white, size: 20),
+            ),
+          ),
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.symmetric(vertical: 12),
+        ),
+      ),
+    );
+  }
+}
