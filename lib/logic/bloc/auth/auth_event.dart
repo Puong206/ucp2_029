206f40b10045c9d6795ac5e11a0f54b5dcd8fe0b
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-11 15:23:34 +0700

    refactor: perbaiki struktur dan penamaan parameter pada event autentikasi

--- lib/logic/bloc/auth/auth_event.dart
+++ lib/logic/bloc/auth/auth_event.dart
@@ -1,20 +1,49 @@
 import 'package:equatable/equatable.dart';
 
-abstract class AuthEvent extends Equatable{
+abstract class AuthEvent extends Equatable {
   @override
   List<Object?> get props => [];
 }
 
+/// Triggered saat app start untuk check apakah user sudah login sebelumnya
 class AppStarted extends AuthEvent {}
 
+/// Triggered saat user submit login form dengan email & password
 class LoginRequested extends AuthEvent {
-  final String email, password;
-  LoginRequested(this.email, this.password);
+  final String email;
+  final String password;
+
+  LoginRequested({
+    required this.email,
+    required this.password,
+  });
+
+  @override
+  List<Object?> get props => [email, password];
 }
 
+/// Triggered saat user submit register form
 class RegisterRequested extends AuthEvent {
-  final String name, email, password;
-  RegisterRequested(this.name, this.email, this.password);
+  final String nama;
+  final String email;
+  final String password;
+
+  RegisterRequested({
+    required this.nama,
+    required this.email,
+    required this.password,
+  });
+
+  @override
+  List<Object?> get props => [nama, email, password];
 }
 
+/// Triggered saat user klik logout button
 class LogoutRequested extends AuthEvent {}
+
+/// Triggered untuk ambil data user yang sedang login (current user)
+/// Requires valid JWT token
+class GetMeRequested extends AuthEvent {}
+
+/// Triggered untuk check apakah user masih authenticated
+class CheckAuthenticationStatus extends AuthEvent {}
