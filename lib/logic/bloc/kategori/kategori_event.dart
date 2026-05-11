ceaac2f4643d83f41b66aaaaf84febec19be35c2
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-11 21:09:08 +0700

    refactor: ubah kelas KategoriEvent menjadi abstract dan perbaiki implementasi kelas turunannya

--- lib/logic/bloc/kategori/kategori_event.dart
+++ lib/logic/bloc/kategori/kategori_event.dart
@@ -1,8 +1,35 @@
 part of 'kategori_bloc.dart';
 
-sealed class KategoriEvent extends Equatable {
+abstract class KategoriEvent extends Equatable {
   const KategoriEvent();
 
   @override
   List<Object> get props => [];
+}
+
+class FetchKategori extends KategoriEvent {}
+
+class CreateKategori extends KategoriEvent {
+  final Map<String, dynamic> data;
+  CreateKategori({required this.data});
+  
+  @override
+  List<Object> get props => [data];
+}
+
+class UpdateKategori extends KategoriEvent {
+  final int id;
+  final Map<String, dynamic> data;
+  UpdateKategori({required this.id, required this.data});
+  
+  @override
+  List<Object> get props => [id, data];
+}
+
+class DeleteKategori extends KategoriEvent {
+  final int id;
+  DeleteKategori({required this.id});
+  
+  @override
+  List<Object> get props => [id];
 }
