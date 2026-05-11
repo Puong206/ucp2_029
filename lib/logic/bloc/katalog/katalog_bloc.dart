d1f6f47fc6bd7b464fa0f41ed6ebd8937e776261
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-11 19:46:25 +0700

    refactor: ubah KatalogState menjadi abstract class dan sesuaikan kelas turunannya

--- lib/logic/bloc/katalog/katalog_bloc.dart
+++ lib/logic/bloc/katalog/katalog_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:ucp2/data/models/katalog_model.dart';
 
 part 'katalog_event.dart';
 part 'katalog_state.dart';
--- lib/logic/bloc/katalog/katalog_state.dart
+++ lib/logic/bloc/katalog/katalog_state.dart
@@ -1,10 +1,26 @@
 part of 'katalog_bloc.dart';
 
-sealed class KatalogState extends Equatable {
-  const KatalogState();
+abstract class KatalogState extends Equatable {
   
   @override
   List<Object> get props => [];
 }
 
-final class KatalogInitial extends KatalogState {}
+class KatalogInitial extends KatalogState {}
+
+class KatalogLoading extends KatalogState {}
+
+class KatalogLoaded extends KatalogState {
+  final List<KatalogModel> katalogList;
+  KatalogLoaded(this.katalogList);
+
+  @override
+  List<Object> get props => [katalogList];
+}
+
+class KatalogError extends KatalogState {
+  final String message;
+  KatalogError(this.message);
+}
+
+class KatalogCreatedSuccess extends KatalogState {}
