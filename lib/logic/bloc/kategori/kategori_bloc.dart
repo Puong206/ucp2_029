0ff1e2173c40d1450accc190aef66b4066a2fa82
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-11 21:09:13 +0700

    refactor: perbaiki implementasi KategoriBloc dengan menambahkan repository dan event handler

--- lib/logic/bloc/kategori/kategori_bloc.dart
+++ lib/logic/bloc/kategori/kategori_bloc.dart
@@ -1,13 +1,56 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:ucp2/data/models/kategori_model.dart';
+import 'package:ucp2/data/repositories/kategori_repository.dart';
 
 part 'kategori_event.dart';
 part 'kategori_state.dart';
 
 class KategoriBloc extends Bloc<KategoriEvent, KategoriState> {
-  KategoriBloc() : super(KategoriInitial()) {
-    on<KategoriEvent>((event, emit) {
-      // TODO: implement event handler
+  final KategoriRepository repository;
+  
+  KategoriBloc({required this.repository}) : super(KategoriInitial()) {
+    on<KategoriEvent>((event, emit) async{
+      emit(KategoriLoading());
+      try {
+        final list = await repository.getAllKategori();
+        emit(KategoriLoaded(list));
+      } catch (e) {
+        emit(KategoriError(e.toString()));
+      }
+    });
+
+    on<CreateKategori>((event, emit) async {
+      emit(KategoriLoading());
+      try {
+        await repository.createKategori(event.data);
+        emit(KategoriCreatedSuccess());
+        add(FetchKategori());
+      } catch (e) {
+        emit(KategoriError(e.toString()));
+      }
+    });
+
+    on<UpdateKategori>((event, emit) async {
+      emit(KategoriLoading());
+      try {
+        await repository.updateKategori(event.id, event.data);
+        emit(KategoriCreatedSuccess());
+        add(FetchKategori());
+      } catch (e) {
+        emit(KategoriError(e.toString()));
+      }
+    });
+
+    on<DeleteKategori>((event, emit) async{
+      emit(KategoriLoading());
+      try {
+        await repository.deleteKategori(event.id);
+        emit(KategoriCreatedSuccess());
+        add(FetchKategori());
+      } catch (e) {
+        emit(KategoriError(e.toString()));
+      }
     });
   }
 }
