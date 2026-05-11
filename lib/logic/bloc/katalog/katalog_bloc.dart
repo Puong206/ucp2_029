78d4440615211cad5cc87c763fd2fe34138cca52
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-11 20:07:53 +0700

    refactor: perbaiki implementasi KatalogBloc dengan menambahkan repository dan event handler

--- lib/logic/bloc/katalog/katalog_bloc.dart
+++ lib/logic/bloc/katalog/katalog_bloc.dart
@@ -1,14 +1,55 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:ucp2/data/models/katalog_model.dart';
+import 'package:ucp2/data/repositories/katalog_repository.dart';
 
 part 'katalog_event.dart';
 part 'katalog_state.dart';
 
 class KatalogBloc extends Bloc<KatalogEvent, KatalogState> {
-  KatalogBloc() : super(KatalogInitial()) {
-    on<KatalogEvent>((event, emit) {
-      // TODO: implement event handler
+  final KatalogRepository repository;
+  
+  KatalogBloc({required this.repository}) : super(KatalogInitial()) {
+    on<FetchKatalog>((event, emit) async {
+      emit(KatalogLoading());
+      try {
+        final list = await repository.getAllKatalog();
+        emit(KatalogLoaded(list));
+      } catch (e) {
+        emit(KatalogError(e.toString()));
+      }
+    });
+
+    on<CreateKatalog>((event, emit) async {
+      emit(KatalogLoading());
+      try {
+        await repository.createKatalog(event.data);
+        emit(KatalogCreatedSuccess());
+        add(FetchKatalog());
+      } catch (e) {
+        emit(KatalogError(e.toString()));
+      }
+    });
+
+    on<UpdateKatalog>((event, emit) async {
+      emit(KatalogLoading());
+      try {
+        await repository.updateKatalog(event.id, event.data);
+        emit(KatalogCreatedSuccess());
+        add(FetchKatalog());
+      } catch (e) {
+        emit(KatalogError(e.toString()));
+      }
+    });
+    on<DeleteKatalog>((event, emit) async{
+      emit(KatalogLoading());
+      try {
+        await repository.deleteKatalog(event.id);
+        emit(KatalogCreatedSuccess());
+        add(FetchKatalog());
+      } catch (e) {
+        emit(KatalogError(e.toString()));
+      }
     });
   }
 }
