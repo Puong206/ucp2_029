54364d8fb6e7cc2220348c0815aaced7b564b926
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-11 15:16:17 +0700

    fix: perbaiki endpoint dan parameter pada metode login dan register

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'dart:developer' as developer;
 import 'package:http/http.dart' as http;
 import 'package:ucp2/data/models/user_model.dart';
+import 'package:ucp2/data/providers/storage_provider.dart';
 
 class AuthRepository {
-  final String baseUrl = "https://ternak-be-production.up.railway.app/api/v1";
+  final String baseUrl = "http://";
   final StorageProvider _storage = StorageProvider();
 
   Future<void> persistToken(String token) async {
@@ -19,41 +21,49 @@ class AuthRepository {
     await _storage.deleteToken();
   }
 
-  Future<UserModel> login(String email, String password) async {
+  Future<Usermodel> login(String email, String password) async {
     try {
       final response = await http.post(
-        Uri.parse('$baseUrl/auth/login'),
+        Uri.parse('$baseUrl/login'),
         headers: {
           'Content-Type': 'application/json',
-          'Accept': 'application/json',
+          'Accept': 'application/json'
         },
-        body: jsonEncode({'email': email, 'password': password}),
+        body: jsonEncode({
+          'email': email,
+          'password': password,
+        }),
       );
 
       final data = jsonDecode(response.body);
-      developer.log('Response Login: ${response.body}', name: 'API');
+      developer.log('Login Response: ${response.body}', name: 'API');
 
       if (response.statusCode == 200) {
         await persistToken(data['token']);
-        return UserModel.fromJson(data['user']);
+        return Usermodel.fromJson(data['user']);
       } else {
-        throw data['message'] ?? 'Gagal Login';
+        throw data['message'] ?? 'Gagal Login!';
       }
+
     } catch (e) {
-      developer.log('Error Login: $e', name: 'API');
+      developer.log('Login Error: $e', name: 'API');
       rethrow;
     }
   }
 
-  Future<void> register(String username, String email, String password) async {
+  Future<void> register(
+    String nama,
+    String email,
+    String password,
+  ) async {
     final response = await http.post(
-      Uri.parse('$baseUrl/auth/register'),
+      Uri.parse('$baseUrl/register'),
       headers: {
         'Content-Type': 'application/json',
-        'Accept': 'application/json',
+        'Accept': 'application/json'
       },
       body: jsonEncode({
-        'username': username,
+        'nama': nama,
         'email': email,
         'password': password,
       }),
@@ -61,7 +71,7 @@ class AuthRepository {
 
     if (response.statusCode != 201 && response.statusCode != 200) {
       final data = jsonDecode(response.body);
-      throw data['message'] ?? 'Gagal Registrasi';
+      throw data['message'] ?? 'Gagal Register!';
     }
   }
 }
