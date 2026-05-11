738218e5375fdba2545a7e5fec0d915d0487703f
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-11 21:09:01 +0700

    refactor: ubah kelas KategoriState menjadi abstract dan perbaiki implementasi kelas turunannya

--- lib/logic/bloc/kategori/kategori_state.dart
+++ lib/logic/bloc/kategori/kategori_state.dart
@@ -1,10 +1,30 @@
 part of 'kategori_bloc.dart';
 
-sealed class KategoriState extends Equatable {
+abstract class KategoriState extends Equatable {
   const KategoriState();
   
   @override
   List<Object> get props => [];
 }
 
-final class KategoriInitial extends KategoriState {}
+class KategoriInitial extends KategoriState {}
+
+class KategoriLoading extends KategoriState {}
+
+class KategoriLoaded extends KategoriState {
+  final List<KategoriModel> kategoriList;
+  KategoriLoaded(this.kategoriList);
+
+  @override
+  List<Object> get props => [kategoriList];
+}
+
+class KategoriError extends KategoriState {
+  final String message;
+  KategoriError(this.message);
+  
+  @override
+  List<Object> get props => [message];
+}
+
+class KategoriCreatedSuccess extends KategoriState {}
