0d25f64856086e59961a8736853db416e470dd04
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-15 03:03:18 +0700

    feat: menambah multer middleware untuk upload image pada katalog routes

--- backend/routes/katalogRoutes.js
+++ backend/routes/katalogRoutes.js
@@ -3,14 +3,16 @@ const router = express.Router();
 
 const katalogController = require('../controllers/katalogController');
 const { verifyToken } = require('../middlewares/authMiddleware');
+const upload = require('../middlewares/upload');
 
 // Public routes (Read only)
 router.get('/', katalogController.getAllKatalog);
 router.get('/:id', katalogController.getKatalogById);
 
 // Protected routes (Create, Update, Delete)
-router.post('/', verifyToken, katalogController.createKatalog);
-router.put('/:id', verifyToken, katalogController.updateKatalog);
+// Protected routes (Create, Update, Delete)
+router.post('/', verifyToken, upload.single('image'), katalogController.createKatalog);
+router.put('/:id', verifyToken, upload.single('image'), katalogController.updateKatalog);
 router.delete('/:id', verifyToken, katalogController.deleteKatalog);
 
 module.exports = router;
